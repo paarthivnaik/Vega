8b0d4e9d955189b4c69a3c4db1e0024590f8b7cc
BalajiNaik <mvgnaik@gmail.com>
2018-10-21 09:35:37 +0530

    Added Update Method

--- Vega/Controllers/VehiclesController.cs
+++ Vega/Controllers/VehiclesController.cs
@@ -17,7 +17,7 @@ namespace Vega.Controllers
     {
         private readonly VegaDbContext context;
         private readonly IMapper mapper;
-        public VehiclesController(IMapper mapper,VegaDbContext context)
+        public VehiclesController(IMapper mapper, VegaDbContext context)
         {
             this.mapper = mapper;
             this.context = context;
@@ -26,13 +26,31 @@ namespace Vega.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateVehicle([FromBody] VehicleResource vehicleResource)
         {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
 
             var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
-
+            vehicle.LastUpdate = DateTime.Now;
             context.Vehicles.Add(vehicle);
             await context.SaveChangesAsync();
-            return Ok(vehicle);
-                
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+            return Ok(result);
+
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateVehicle(int id,[FromBody] VehicleResource vehicleResource)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var vehicle = await context.Vehicles.FindAsync(id);
+            mapper.Map<VehicleResource, Vehicle>(vehicleResource, vehicle);
+            vehicle.LastUpdate = DateTime.Now;
+            await context.SaveChangesAsync();
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+            return Ok(result);
+
         }
     }
 }
--- Vega/Mapping/MappingProfile.cs
+++ Vega/Mapping/MappingProfile.cs
@@ -13,8 +13,12 @@ namespace Vega.Mapping
             CreateMap<Make, MakeResource>();
             CreateMap<Model, ModelResource>();
             CreateMap<Feature, FeatureResource>();
+            CreateMap<Vehicle, VehicleResource>()
+               .ForMember(v => v.Contact, opt => opt.MapFrom(v => new ContactResource { ContactName = v.ContactName, ContactEmail = v.ContactEmail, ContactPhone = v.ContactPhone }))
+                 .ForMember(v => v.Features, opt => opt.MapFrom(v => v.VehicleFeatures.Select(vf => vf.FeatureId)));
             //API Resource to Domain Resource
             CreateMap<VehicleResource, Vehicle>()
+                .ForMember(v=>v.Id,opt=>opt.Ignore())
                 .ForMember(v => v.ContactName, opt => opt.MapFrom(v => v.Contact.ContactName))
                    .ForMember(v => v.ContactEmail, opt => opt.MapFrom(v => v.Contact.ContactEmail))
                       .ForMember(v => v.ContactPhone, opt => opt.MapFrom(v => v.Contact.ContactPhone))
